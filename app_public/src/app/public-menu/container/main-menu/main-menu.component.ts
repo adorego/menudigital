d8bddbbdd604084ddb01820f2fe9c88624f5ae33
Andres Do Rego Barros <adoregoel@gmail.com>
2021-03-12 00:50:56 -0300

    Menu Principal con tamaños, sabores y toppings

--- app_public/src/app/public-menu/container/main-menu/main-menu.component.ts
+++ app_public/src/app/public-menu/container/main-menu/main-menu.component.ts
@@ -2,8 +2,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { CategoriaMenu } from 'src/app/core/models/categoria-menu.model';
+import { CostoAdicionalSabor } from 'src/app/core/models/costo-adicional-sabor.model';
 import { Local } from 'src/app/core/models/local.model';
 import { MenuItem } from 'src/app/core/models/menuItem.model';
+import { OpcionSabor } from 'src/app/core/models/opcion-sabor.model';
 import { PublicMenuFacade } from '../../services/publicMenuFacade.service';
 
 @Component({
@@ -56,6 +58,30 @@ export class MainMenuComponent implements OnInit, OnDestroy {
     console.log('Agregar a carrito,', menuitem, 'cantidad:', this.cantidad);
   }
 
+  public costoAdicional(sabor:OpcionSabor, tamanoid:string):CostoAdicionalSabor{
+    let encontrado:CostoAdicionalSabor;
+    if(sabor.costoAdicional != null && sabor.costoAdicional.length>0){
+        sabor.costoAdicional.find(
+        (costoAdicional) => {
+          if(costoAdicional.tamano == tamanoid )
+           encontrado = costoAdicional;
+        });
+      return encontrado;  
+    }else{
+      return null;
+    }
+
+  }
+  public operacion(sabor:OpcionSabor, tamanoid:string){
+    let costoAdicionalSabor:CostoAdicionalSabor = this.costoAdicional(sabor, tamanoid);
+    if(costoAdicionalSabor.operacion ==1){
+      return '+';
+    }else if(costoAdicionalSabor.operacion == 2){
+      return '-';
+    }else{
+      return '';
+    }
+  }
   public handleMinus() {
     this.cantidad--;  
   }
